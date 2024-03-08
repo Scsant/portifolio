3ff385f22447db7c82306a80327cef4fab380231
Scsant <110061187+Scsant@users.noreply.github.com>
2024-03-08 16:58:32 -0300

    Add files via upload

---
+++ repositorio/script.js
@@ -0,0 +1,11 @@
+// Função para abrir o popup
+function abrirPopup() {
+    var popup = document.getElementById("meuPopup");
+    popup.classList.add("mostrar");
+}
+
+// Função para fechar o popup
+function fecharPopup() {
+    var popup = document.getElementById("meuPopup");
+    popup.classList.remove("mostrar");
+}
