65ff630600b72b356fe41d0d8ae023411cf4ce02
Scsant <110061187+Scsant@users.noreply.github.com>
2024-04-01 16:33:21 -0300

    Add files via upload

--- repositorio/script.js
+++ repositorio/script.js
@@ -9,3 +9,20 @@ function fecharPopup() {
     var popup = document.getElementById("meuPopup");
     popup.classList.remove("mostrar");
 }
+function mudarImagem() {
+    var imagem = document.getElementById('imagemProjeto');
+    if (imagem.src.match('interfaceAutSap.png')) {
+        imagem.src = 'interfaceAutSapII.png';
+    } else {
+        imagem.src = 'interfaceAutSap.png';
+    }
+}
+
+function mudarImagemI() {
+    var imagem = document.getElementById('imagemProjetoI');
+    if (imagem.src.match('')) {
+        imagem.src = 'interfaceAutSapII.png';
+    } else {
+        imagem.src = 'interfaceAutSap.png';
+    }
+}
